fc839a5d7f6de78ce9457a954afb100b6f9cc948
ctmurphy77 <rajonwitness@gmail.com>
2018-09-25 14:35:55 -0400

    Added 404 and 500 error pages

--- server.js
+++ server.js
@@ -98,10 +98,18 @@ app.get('/pagecount', function (req, res) {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
-// error handling
+
+// 500 error handler (middleware)
 app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
+ console.error(err.stack);
+ res.status(500);
+ res.render('500');
+});
+
+// 404 catch-all handler (middleware)
+app.use(function(req, res, next){
+ res.status(404);
+ res.render('404');
 });
 
 initDb(function(err){
